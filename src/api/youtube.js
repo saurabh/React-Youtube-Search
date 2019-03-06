e0a8263c11f9c68c0b5989fb17bc758e93caf2e8
Saurabh Wadhwa <w.saurabh@protonmail.com>
2019-03-06 21:36:25 +0400

    Update youtube.js

--- src/api/youtube.js
+++ src/api/youtube.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const KEY = 'AIzaSyDCPqDUpseoEhWFejNv9ya6dIY5Ky_pnuo'; //access restricted to localhost and react-tube.surge.sh via googleDev console.
+const KEY = 'AIzaSyDCPqDUpseoEhWFejNv9ya6dIY5Ky_pnuo'; //access restricted to localhost and react-tube.surge.sh
 
 export default axios.create({
     baseURL: 'https://www.googleapis.com/youtube/v3',
